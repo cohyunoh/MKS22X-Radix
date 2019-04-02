280a99b61352292769d2626fea4212e7c477314c
cohyunoh <coh00@stuy.edu>
2019-04-02 11:31:45 -0400

    testing to see if i can actually grab the correct digit and it was successful

--- Radix.java
+++ Radix.java
@@ -3,8 +3,10 @@ public class Radix{
     @SuppressWarnings({"unchecked" , "rawtypes"})
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
     int largestDigit = getLargest(data) + "".length();
-    largestDigit =  (int)Math.pow(10, largestDigit - 1);
-    
+    MyLinkedList<Integer> list = new MyLinkedList<Integer>();
+    //go through first digits
+    int bucketIndex = 0;
+
   }
 
   private static int getLargest(int[]data){
@@ -15,5 +17,24 @@ public class Radix{
       }
     }
     return ans;
+  }
+
+  private static int putIntoBucket(int digit, int value){
+    int dig = (int)Math.pow(10 , digit);
+    value =  value / dig;
+    value = value % 10;
+    return value;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(putIntoBucket(0, 987654321));
+    System.out.println(putIntoBucket(1, 987654321));
+    System.out.println(putIntoBucket(2, 987654321));
+    System.out.println(putIntoBucket(3, 987654321));
+    System.out.println(putIntoBucket(4, 987654321));
+    System.out.println(putIntoBucket(5, 987654321));
+    System.out.println(putIntoBucket(6, 987654321));
+    System.out.println(putIntoBucket(7, 987654321));
+    System.out.println(putIntoBucket(8, 987654321));
   }
 }
