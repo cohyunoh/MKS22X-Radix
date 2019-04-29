6d4970e42c0a2d29023d2c3648cf915403a76a25
cohyunoh <coh00@stuy.edu>
2019-04-29 18:59:49 -0400

    still too slow

--- Radix.java
+++ Radix.java
@@ -1,15 +1,45 @@
 import java.util.Arrays;
 public class Radix{
+  @SuppressWarnings({"unchecked" , "rawtypes"})
+  private static MyLinkedList<Integer>[] buckets = new MyLinkedList[19];
   public static void radixsort(int[]data){
     if(data.length == 0){
       return ;
     }
-    @SuppressWarnings({"unchecked" , "rawtypes"})
-    MyLinkedList<Integer>[] buckets = new MyLinkedList[19];
-    int bucketIndex = 0;
+    int digitIndex = 0;
     int MAX = getLargest(data) + "".length();
+    while(digitIndex < MAX){
+      radixHelper(digitIndex, data);
+      digitIndex++;
+    }
   }
 
+  private static void radixHelper(int digitIndex, int[]data){
+    MyLinkedList<Integer> list = new MyLinkedList<Integer>();
+    for(int i = 0; i < data.length; i++){
+      //get the digit
+      int dig = getDigit(digitIndex, data[i]);
+      //if the number is negative
+      if(data[i] < 0){
+        //make sure the bucket we are adding to is instantiated
+        if(buckets[9-dig] == null){
+          buckets[9-dig] = new MyLinkedList<Integer>();
+        }
+        //add it to the index of 9 minus the digit
+        buckets[9-dig].add(data[i]);
+      //if the number is positive
+    }else if(data[i] >= 0){
+        //make sure the bucket we are adding to is instantiated
+        if(buckets[dig + 9] == null){
+          buckets[dig + 9] = new MyLinkedList<Integer>();
+        }
+        //add it to the index of 10 plus the digit
+        buckets[dig + 9].add(data[i]);
+      }
+    }
+    //transfer the values in order to data
+    combine(buckets, data);
+  }
   private static int getLargest(int[]data){
     //starting with the first element, compare the ans variable to all the elements to find th greatest value
     int ans = data[0];
@@ -32,13 +62,16 @@ public class Radix{
     return value;
   }
 
-  private static void combine(MyLinkedList<Integer> list, MyLinkedList<Integer>[] buckets){
+  private static void combine(MyLinkedList<Integer>[] buckets, int[] data){
     //iterates through the buckets array and list extends those linkedlists in each bucket
+    int index = 0;
     for(int i = 0; i < buckets.length; i++){
       if(buckets[i] != null){
-        if(buckets[i].size() != 0){
-          list.extend(buckets[i]);
-          buckets[i].clear();
+        int size = buckets[i].size();
+        while(size > 0){
+          data[index] = (int)buckets[i].removeFront();
+          index ++;
+          size --;
         }
       }
     }
