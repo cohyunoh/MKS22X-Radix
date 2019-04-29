eb0a32d197195c9019a33b95164f1df0945165ec
cohyunoh <coh00@stuy.edu>
2019-04-29 18:41:23 -0400

    trying to redo radixsort

--- Radix.java
+++ Radix.java
@@ -5,75 +5,9 @@ public class Radix{
       return ;
     }
     @SuppressWarnings({"unchecked" , "rawtypes"})
-    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
-    //this is the highest digit in the list
-    int largestDigit = getLargest(data) + "".length();
-    //this will store the values after each pass
-    MyLinkedList<Integer> list = new MyLinkedList<Integer>();
-    //go through first digits
-    //this will keep track of which digit we looking at
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[19];
     int bucketIndex = 0;
-    //for the first pass through transfer from data to buckets then to list
-    for(int i = 0; i < data.length; i++){
-      //get the digit
-      int dig = getDigit(bucketIndex, data[i]);
-      //if the number is negative
-      if(data[i] < 0){
-        //make sure the bucket we are adding to is instantiated
-        if(buckets[9-dig] == null){
-          buckets[9-dig] = new MyLinkedList<Integer>();
-        }
-        //add it to the index of 9 minus the digit
-        buckets[9-dig].add(data[i]);
-      //if the number is positive
-    }else if(data[i] >= 0){
-        //make sure the bucket we are adding to is instantiated
-        if(buckets[dig + 10] == null){
-          buckets[dig + 10] = new MyLinkedList<Integer>();
-        }
-        //add it to the index of 10 plus the digit
-        buckets[dig + 10].add(data[i]);
-      }
-    }
-    //transfer the values in order to list
-    combine(list,buckets);
-    //go to the next digit
-    bucketIndex ++;
-    //keep running until we reach the highest digit
-    while(bucketIndex < largestDigit){
-      int size = list.size();
-      //System.out.println(list);
-      for(int i = 0; i < size; i++){
-        //get the first value of the list and remove it at the same time
-        Integer val = list.removeFront();
-        //get the desired digit
-        int dig = getDigit(bucketIndex, val);
-        //if the number is positive
-        if(val < 0){
-          //make sure the bucket we are adding to is instantiated
-          if(buckets[9-dig] == null){
-            //add it to the index of 9 minus the digit
-            buckets[9-dig] = new MyLinkedList<Integer>();
-          }
-
-          buckets[9-dig].add(val);
-        }else if(val >= 0){
-          //make sure the bucket we are adding to is instantiated
-          if(buckets[dig + 10] == null){
-            buckets[dig + 10] = new MyLinkedList<Integer>();
-          }
-          //add it to the index of 10 plus the digit
-          buckets[dig + 10].add(val);
-        }
-      }
-      //transfer the values in order to list
-      combine(list,buckets);
-      bucketIndex ++;
-      bucketIndex ++;
-    }
-    //System.out.println(list);
-    //transfer all the values from list to data
-    combine(list, data);
+    int MAX = getLargest(data) + "".length();
   }
 
   private static int getLargest(int[]data){
